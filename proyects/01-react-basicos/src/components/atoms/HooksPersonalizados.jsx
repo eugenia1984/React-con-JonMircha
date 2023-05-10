a308c85ea25fb3342e098163d20c4f0b3c505427
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-05-10 10:15:32 -0300

    Agrego el hook personalizado

--- proyects/01-react-basicos/src/components/atoms/HooksPersonalizados.jsx
+++ proyects/01-react-basicos/src/components/atoms/HooksPersonalizados.jsx
@@ -1,4 +1,4 @@
-import {useFetch} from '../../hooks/useFetch'
+import { useFetch } from '../../hooks/useFetch'
 
 export default function HooksPersonalizados() {
   const url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -8,8 +8,14 @@ export default function HooksPersonalizados() {
     <>
       <h2>Custom hooks</h2>
       <h3>isPending = {JSON.stringify(isPending)}</h3>
-      <h3>error = <mark>{JSON.stringify(error)}</mark></h3>
-      <h3><p>{JSON.stringify(data)}</p></h3>
+      <h3>
+        error = <mark>{JSON.stringify(error)}</mark>
+      </h3>
+      <h3>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>
+          <code>{JSON.stringify(data)}</code>
+        </pre>
+      </h3>
       <hr />
     </>
   )
