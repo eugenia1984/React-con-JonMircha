971ea19e145968dd87ac3237f19e52982eb22228
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-05-12 10:02:21 -0300

    Saco los estilos

--- proyects/02-frameworks-css/src/App.jsx
+++ proyects/02-frameworks-css/src/App.jsx
@@ -8,15 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Frameworks</h1>
+      <header>
+        <h1>Frameworks CSS with React</h1>
+      </header>
     </>
   )
 }
