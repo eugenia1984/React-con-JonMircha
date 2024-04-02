62212ce68dbd9c37e545fac5924fbf378ae8e714
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-04-02 15:38:27 -0300

    styles: cambio los logos

--- proyectos/01-react-basics/src/App.jsx
+++ proyectos/01-react-basics/src/App.jsx
@@ -1,39 +1,43 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-import Componente from './components/atoms/Componente'
-import Estado from './components/atoms/Estado'
-import RenderizadoCondicional from './components/atoms/RenderizadoCondicional'
-import RenderizadoElementos from './components/atoms/RendrizadoElementos'
-import EventosES6 from './components/atoms/EventosES6'
-import EventosES7, { MasSobreEventos } from './components/atoms/EventosES7'
-import ComunicacionComponentes from './components/atoms/ComunicacionComponentes'
-import CicloVida from './components/atoms/CicloVida'
-import AjaxApis from './components/atoms/AjaxApi'
-import ContadorHooks from './components/atoms/ContadorHooks'
-import ScrollHooks from './components/atoms/ScrollHooks'
-import RelojHooks from './components/atoms/RelojHooks'
-import AjaxHooks from './components/atoms/AjaxHooks'
-import HooksPersonalizados from './components/atoms/HooksPersonalizados'
-import Referencias from './components/atoms/REferencias'
-import Formularios from './components/atoms/Formularios'
-import Estilos from './components/atoms/estilos/Estilos'
-import ComponenteEstilizado from './components/atoms/estilos/ComponentesEstilizados'
-import { Authorized, DisplayName, NameForm, Propiedades, Seasons } from './components/atoms'
+import Componente from "./components/atoms/Componente";
+import Estado from "./components/atoms/Estado";
+import RenderizadoCondicional from "./components/atoms/RenderizadoCondicional";
+import RenderizadoElementos from "./components/atoms/RendrizadoElementos";
+import EventosES6 from "./components/atoms/EventosES6";
+import EventosES7, { MasSobreEventos } from "./components/atoms/EventosES7";
+import ComunicacionComponentes from "./components/atoms/ComunicacionComponentes";
+import CicloVida from "./components/atoms/CicloVida";
+import AjaxApis from "./components/atoms/AjaxApi";
+import ContadorHooks from "./components/atoms/ContadorHooks";
+import ScrollHooks from "./components/atoms/ScrollHooks";
+import RelojHooks from "./components/atoms/RelojHooks";
+import AjaxHooks from "./components/atoms/AjaxHooks";
+import HooksPersonalizados from "./components/atoms/HooksPersonalizados";
+import Referencias from "./components/atoms/REferencias";
+import Formularios from "./components/atoms/Formularios";
+import Estilos from "./components/atoms/estilos/Estilos";
+import ComponenteEstilizado from "./components/atoms/estilos/ComponentesEstilizados";
+import {
+  Authorized,
+  DisplayName,
+  NameForm,
+  Propiedades,
+  Seasons,
+} from "./components/atoms";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <header>
-        <h1>01-React Basics</h1>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          <img src={viteLogo} alt="Vite logo" width="30" className="logo" />
+          01 - React Basics
+          <img src={reactLogo} width="30" alt="React logo" className="logo" />
+        </h1>
       </header>
       <main>
         <NameForm />
@@ -48,8 +52,8 @@ function App() {
           booleano={true}
           arreglo={[1, 2, 3]}
           objeto={{
-            nombre: 'María Eugenia Costa',
-            correo: 'costamariaeugenia1@gmail.com'
+            nombre: "María Eugenia Costa",
+            correo: "costamariaeugenia1@gmail.com",
           }}
           funcion={(num) => num * num}
           elementoReact={<i>This is a React Element</i>}
@@ -75,7 +79,7 @@ function App() {
         <ComponenteEstilizado />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
