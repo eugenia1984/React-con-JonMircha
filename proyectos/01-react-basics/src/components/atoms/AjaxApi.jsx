ec0ea0a19c87ba2ad48f31e72c7f4ffb58dafe71
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-04-03 14:16:39 -0300

    feat: ayax basicos

--- proyectos/01-react-basics/src/components/atoms/AjaxApi.jsx
+++ proyectos/01-react-basics/src/components/atoms/AjaxApi.jsx
@@ -1,41 +1,42 @@
-import { Component } from 'react'
+/* eslint-disable react/prop-types */
+import { Component } from "react";
 
 function Pokemon({ avatar, name }) {
   return (
-    <figure style={{display: 'inline-block'}}>
+    <figure style={{ display: "inline-block" }}>
       <img src={avatar} alt={name} />
       <figcaption>{name}</figcaption>
     </figure>
-  )
+  );
 }
 
 export default class AjaxApis extends Component {
   state = {
-    pokemons: []
-  }
+    pokemons: [],
+  };
 
   componentDidMount() {
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
+    const url = "https://pokeapi.co/api/v2/pokemon/";
     fetch(url)
       .then((res) => res.json())
       .then((json) => {
         json.results.forEach((el) => {
           fetch(el.url)
-          // Por cada pokemon un fetch para obtener la imagen
+            // Por cada pokemon un fetch para obtener la imagen
             .then((res) => res.json())
             .then((json) => {
               const pokemon = {
                 id: json.id,
                 name: json.name,
-                avatar: json.sprites.front_default
-              }
+                avatar: json.sprites.front_default,
+              };
 
-              const pokemons = [...this.state.pokemons, pokemon]
+              const pokemons = [...this.state.pokemons, pokemon];
 
-              this.setState({ pokemons })
-            })
-        })
-      })
+              this.setState({ pokemons });
+            });
+        });
+      });
   }
 
   render() {
@@ -43,14 +44,18 @@ export default class AjaxApis extends Component {
       <>
         <h2>Asynchronous Requests in Class Components</h2>
         {this.state.pokemons.length === 0 ? (
-          <h3>Loading...</h3>
+          <h3>Loading pokemons...</h3>
         ) : (
           this.state.pokemons.map((el) => (
-            <Pokemon key={el.id} name={el.name} avatar={el.avatar} />
+            <Pokemon
+              key={`${el.id}-${el.name}`}
+              name={el.name}
+              avatar={el.avatar}
+            />
           ))
         )}
         <hr />
       </>
-    )
+    );
   }
 }
